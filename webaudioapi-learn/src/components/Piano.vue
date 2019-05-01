6e38ce43fd82c2d433ef450bb7c812b926faa009
st843391622 <843391622@qq.com>
2019-05-02 00:24:23 +0800

    update 5.2
    添加按键监听

--- webaudioapi-learn/src/components/Piano.vue
+++ webaudioapi-learn/src/components/Piano.vue
@@ -1,43 +1,12 @@
 <template>
   <div class="piano">
-    <div class="k wk" v-html="keys[0].hname" @click="start(keys[0].mname)"></div>
-    <div class="k bk" v-html="keys[1].hname" @click="start(keys[1].mname)"></div>
-    <div class="k wk" v-html="keys[2].hname" @click="start(keys[2].mname)"></div>
-    <div class="k bk" v-html="keys[3].hname" @click="start(keys[3].mname)"></div>
-    <div class="k wk" v-html="keys[4].hname" @click="start(keys[4].mname)"></div>
-    <div class="k wk" v-html="keys[5].hname" @click="start(keys[5].mname)"></div>
-    <div class="k bk" v-html="keys[6].hname" @click="start(keys[6].mname)"></div>
-    <div class="k wk" v-html="keys[7].hname" @click="start(keys[7].mname)"></div>
-    <div class="k bk" v-html="keys[8].hname" @click="start(keys[8].mname)"></div>
-    <div class="k wk" v-html="keys[9].hname" @click="start(keys[9].mname)"></div>
-    <div class="k bk" v-html="keys[10].hname" @click="start(keys[10].mname)"></div>
-    <div class="k wk" v-html="keys[11].hname" @click="start(keys[11].mname)"></div>
-
-    <div class="k wk" v-html="keys[12].hname" @click="start(keys[12].mname)"></div>
-    <div class="k bk" v-html="keys[13].hname" @click="start(keys[13].mname)"></div>
-    <div class="k wk" v-html="keys[14].hname" @click="start(keys[14].mname)"></div>
-    <div class="k bk" v-html="keys[15].hname" @click="start(keys[15].mname)"></div>
-    <div class="k wk" v-html="keys[16].hname" @click="start(keys[16].mname)"></div>
-    <div class="k wk" v-html="keys[17].hname" @click="start(keys[17].mname)"></div>
-    <div class="k bk" v-html="keys[18].hname" @click="start(keys[18].mname)"></div>
-    <div class="k wk" v-html="keys[19].hname" @click="start(keys[19].mname)"></div>
-    <div class="k bk" v-html="keys[20].hname" @click="start(keys[20].mname)"></div>
-    <div class="k wk" v-html="keys[21].hname" @click="start(keys[21].mname)"></div>
-    <div class="k bk" v-html="keys[22].hname" @click="start(keys[22].mname)"></div>
-    <div class="k wk" v-html="keys[23].hname" @click="start(keys[23].mname)"></div>
-
-    <div class="k wk" v-html="keys[24].hname" @click="start(keys[24].mname)"></div>
-    <div class="k bk" v-html="keys[25].hname" @click="start(keys[25].mname)"></div>
-    <div class="k wk" v-html="keys[26].hname" @click="start(keys[26].mname)"></div>
-    <div class="k bk" v-html="keys[27].hname" @click="start(keys[27].mname)"></div>
-    <div class="k wk" v-html="keys[28].hname" @click="start(keys[28].mname)"></div>
-    <div class="k wk" v-html="keys[29].hname" @click="start(keys[29].mname)"></div>
-    <div class="k bk" v-html="keys[30].hname" @click="start(keys[30].mname)"></div>
-    <div class="k wk" v-html="keys[31].hname" @click="start(keys[31].mname)"></div>
-    <div class="k bk" v-html="keys[32].hname" @click="start(keys[32].mname)"></div>
-    <div class="k wk" v-html="keys[33].hname" @click="start(keys[33].mname)"></div>
-    <div class="k bk" v-html="keys[34].hname" @click="start(keys[34].mname)"></div>
-    <div class="k wk" v-html="keys[35].hname" @click="start(keys[35].mname)"></div>
+    <div
+      v-for="skey in keys"
+      :key="skey.mname"
+      :class="skey.kcolor"
+      v-html="skey.hname"
+      @click="start(skey.mname)"
+    ></div>
   </div>
 </template>
 
@@ -49,162 +18,218 @@ export default {
     return {
       keys: [
         {
+          kcolor: "wk",
           hname: "C3",
           mname: "C3"
         },
         {
+          kcolor: "bk",
           hname: "C<sup>#</sup>3",
           mname: "C#3"
         },
         {
+          kcolor: "wk",
           hname: "D3",
           mname: "D3"
         },
         {
+          kcolor: "bk",
           hname: "D<sup>#</sup>3",
           mname: "D#3"
         },
         {
+          kcolor: "wk",
           hname: "E3",
           mname: "E3"
         },
         {
+          kcolor: "wk",
           hname: "F3",
           mname: "F3"
         },
         {
+          kcolor: "bk",
           hname: "F<sup>#</sup>3",
           mname: "F#3"
         },
         {
+          kcolor: "wk",
           hname: "G3",
           mname: "G3"
         },
         {
+          kcolor: "bk",
           hname: "G<sup>#</sup>3",
           mname: "G#3"
         },
         {
+          kcolor: "wk",
           hname: "A3",
           mname: "A3"
         },
         {
+          kcolor: "bk",
           hname: "A<sup>#</sup>3",
           mname: "A#3"
         },
         {
+          kcolor: "wk",
           hname: "B3",
           mname: "B3"
         },
         {
+          kcolor: "wk",
           hname: "C4",
           mname: "C4"
         },
         {
+          kcolor: "bk",
           hname: "C<sup>#</sup>4",
           mname: "C#4"
         },
         {
+          kcolor: "wk",
           hname: "D4",
           mname: "D4"
         },
         {
+          kcolor: "bk",
           hname: "D<sup>#</sup>4",
           mname: "D#4"
         },
         {
+          kcolor: "wk",
           hname: "E4",
           mname: "E4"
         },
         {
+          kcolor: "wk",
           hname: "F4",
           mname: "F4"
         },
         {
+          kcolor: "bk",
           hname: "F<sup>#</sup>4",
           mname: "F#4"
         },
         {
+          kcolor: "wk",
           hname: "G4",
           mname: "G4"
         },
         {
+          kcolor: "bk",
           hname: "G<sup>#</sup>4",
           mname: "G#4"
         },
         {
+          kcolor: "wk",
           hname: "A4",
           mname: "A4"
         },
         {
+          kcolor: "bk",
           hname: "A<sup>#</sup>4",
           mname: "A#4"
         },
         {
+          kcolor: "wk",
           hname: "B4",
           mname: "B4"
         },
         {
+          kcolor: "wk",
           hname: "C5",
           mname: "C5"
         },
         {
+          kcolor: "bk",
           hname: "C<sup>#</sup>5",
           mname: "C#5"
         },
         {
+          kcolor: "wk",
           hname: "D5",
           mname: "D5"
         },
         {
+          kcolor: "bk",
           hname: "D<sup>#</sup>5",
           mname: "D#5"
         },
         {
+          kcolor: "wk",
           hname: "E5",
           mname: "E5"
         },
         {
+          kcolor: "wk",
           hname: "F5",
           mname: "F5"
         },
         {
+          kcolor: "bk",
           hname: "F<sup>#</sup>5",
           mname: "F#5"
         },
         {
+          kcolor: "wk",
           hname: "G5",
           mname: "G5"
         },
         {
+          kcolor: "bk",
           hname: "G<sup>#</sup>5",
           mname: "G#5"
         },
         {
+          kcolor: "wk",
           hname: "A5",
           mname: "A5"
         },
         {
+          kcolor: "bk",
           hname: "A<sup>#</sup>5",
           mname: "A#5"
         },
         {
+          kcolor: "wk",
           hname: "B5",
           mname: "B5"
         }
       ]
     };
   },
-  methods: {
-    start(mname) {
-      console.log(mname);
-      var synth = new Tone.Synth({
-        envelope  : {
-        attack  : 0.01 ,
-        decay  : 0.2 ,
-        sustain  : 0.9 ,
-        release  : 2
+  created() {
+    var _this = this;
+    document.onkeydown = function(e) {
+      let key = window.event.keyCode;
+      if (e.shiftKey) {
+        if (key == 81) {_this.start('C#3');}
+        if (key == 87) {_this.start('D#3');}
+        if (key == 82) {_this.start('F#3');}
+        if (key == 84) {_this.start('G#3');}
+        if (key == 89) {_this.start('A#3');}
+      } else {
+        if (key == 81) {_this.start('C3');}
+        if (key == 87) {_this.start('D3');}
+        if (key == 69) {_this.start('E3');}
+        if (key == 82) {_this.start('F3');}
+        if (key == 84) {_this.start('G3');}
+        if (key == 89) {_this.start('A3');}
+        if (key == 85) {_this.start('B3');}
       }
+    };
+  },
+  methods: {
+    start: function(mname) {
+      var synth = new Tone.Synth({
+        envelope: {
+          attack: 0.01,
+          decay: 0.05,
+          sustain: 0.5,
+          release: 2
+        }
       }).toMaster();
       synth.triggerAttackRelease(mname, "8n");
     }
@@ -221,8 +246,11 @@ export default {
   position: relative;
 }
 .k {
-  border-radius: 0 0 0.3em 0.3em;
+  border-radius: 0 0 0.5em 0.5em;
   text-align: center;
+  &:active {
+    background-color: #4d4d4d;
+  }
 }
 .wk {
   margin: 0 -0.05em;
@@ -232,6 +260,7 @@ export default {
   border: #000 solid 0.1em;
   z-index: 1;
   line-height: 37em;
+  @extend .k;
 }
 .bk {
   margin: 0 -1.5em;
@@ -241,5 +270,6 @@ export default {
   line-height: 22em;
   z-index: 2;
   color: #fff;
+  @extend .k;
 }
 </style>
