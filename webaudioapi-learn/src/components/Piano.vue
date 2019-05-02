8a24cdd7fb2a6410de34ca35361bfa1d613d7709
st843391622 <843391622@qq.com>
2019-05-02 20:40:20 +0800

    update 5.2

--- webaudioapi-learn/src/components/Piano.vue
+++ webaudioapi-learn/src/components/Piano.vue
@@ -1,12 +1,27 @@
 <template>
   <div class="piano">
-    <div
-      v-for="skey in keys"
-      :key="skey.mname"
-      :class="skey.kcolor"
-      v-html="skey.hname"
-      @click="start(skey.mname)"
-    ></div>
+    <div class="keyboard" v-if="mode==-1">
+      <div
+        v-for="(skey, index) in keys"
+        :key="index"
+        :class="skey.kcolor"
+        v-html="skey.hname"
+        @click="start(skey.mname)"
+      ></div>
+    </div>
+    <div class="keyboard" v-else-if="mode==1">
+      <div
+        v-for="(skey, index) in keys"
+        :key="index"
+        :class="skey.kcolor"
+        v-html="skey.kname"
+        @click="start(skey.mname)"
+      ></div>
+    </div>
+    <div class="meau">
+      <input type="checkbox" @click="handle()" id="name">
+      <label for="name">显示按键名</label>
+    </div>
   </div>
 </template>
 
@@ -16,186 +31,223 @@ import Tone from "tone";
 export default {
   data() {
     return {
+      mode:-1,
       keys: [
         {
           kcolor: "wk",
           hname: "C3",
-          mname: "C3"
+          mname: "C3",
+          kname: "q"
         },
         {
           kcolor: "bk",
           hname: "C<sup>#</sup>3",
-          mname: "C#3"
+          mname: "C#3",
+          kname: "↑q"
         },
         {
           kcolor: "wk",
           hname: "D3",
-          mname: "D3"
+          mname: "D3",
+          kname: "w"
         },
         {
           kcolor: "bk",
           hname: "D<sup>#</sup>3",
-          mname: "D#3"
+          mname: "D#3",
+          kname: "↑w"
         },
         {
           kcolor: "wk",
           hname: "E3",
-          mname: "E3"
+          mname: "E3",
+          kname: "e"
         },
         {
           kcolor: "wk",
           hname: "F3",
-          mname: "F3"
+          mname: "F3",
+          kname: "r"
         },
         {
           kcolor: "bk",
           hname: "F<sup>#</sup>3",
-          mname: "F#3"
+          mname: "F#3",
+          kname: "↑r"
         },
         {
           kcolor: "wk",
           hname: "G3",
-          mname: "G3"
+          mname: "G3",
+          kname: "t"
         },
         {
           kcolor: "bk",
           hname: "G<sup>#</sup>3",
-          mname: "G#3"
+          mname: "G#3",
+          kname: "↑t"
         },
         {
           kcolor: "wk",
           hname: "A3",
-          mname: "A3"
+          mname: "A3",
+          kname: "y"
         },
         {
           kcolor: "bk",
           hname: "A<sup>#</sup>3",
-          mname: "A#3"
+          mname: "A#3",
+          kname: "↑y"
         },
         {
           kcolor: "wk",
           hname: "B3",
-          mname: "B3"
+          mname: "B3",
+          kname: "u"
         },
         {
           kcolor: "wk",
           hname: "C4",
-          mname: "C4"
+          mname: "C4",
+          kname: "a"
         },
         {
           kcolor: "bk",
           hname: "C<sup>#</sup>4",
-          mname: "C#4"
+          mname: "C#4",
+          kname: "↑a"
         },
         {
           kcolor: "wk",
           hname: "D4",
-          mname: "D4"
+          mname: "D4",
+          kname: "s"
         },
         {
           kcolor: "bk",
           hname: "D<sup>#</sup>4",
-          mname: "D#4"
+          mname: "D#4",
+          kname: "↑s"
         },
         {
           kcolor: "wk",
           hname: "E4",
-          mname: "E4"
+          mname: "E4",
+          kname: "d"
         },
         {
           kcolor: "wk",
           hname: "F4",
-          mname: "F4"
+          mname: "F4",
+          kname: "f"
         },
         {
           kcolor: "bk",
           hname: "F<sup>#</sup>4",
-          mname: "F#4"
+          mname: "F#4",
+          kname: "↑f"
         },
         {
           kcolor: "wk",
           hname: "G4",
-          mname: "G4"
+          mname: "G4",
+          kname: "g"
         },
         {
           kcolor: "bk",
           hname: "G<sup>#</sup>4",
-          mname: "G#4"
+          mname: "G#4",
+          kname: "↑g"
         },
         {
           kcolor: "wk",
           hname: "A4",
-          mname: "A4"
+          mname: "A4",
+          kname: "h"
         },
         {
           kcolor: "bk",
           hname: "A<sup>#</sup>4",
-          mname: "A#4"
+          mname: "A#4",
+          kname: "↑h"
         },
         {
           kcolor: "wk",
           hname: "B4",
-          mname: "B4"
+          mname: "B4",
+          kname: "j"
         },
         {
           kcolor: "wk",
           hname: "C5",
-          mname: "C5"
+          mname: "C5",
+          kname: "z"
         },
         {
           kcolor: "bk",
           hname: "C<sup>#</sup>5",
-          mname: "C#5"
+          mname: "C#5",
+          kname: "↑z"
         },
         {
           kcolor: "wk",
           hname: "D5",
-          mname: "D5"
+          mname: "D5",
+          kname: "x"
         },
         {
           kcolor: "bk",
           hname: "D<sup>#</sup>5",
-          mname: "D#5"
+          mname: "D#5",
+          kname: "↑x"
         },
         {
           kcolor: "wk",
           hname: "E5",
-          mname: "E5"
+          mname: "E5",
+          kname: "c"
         },
         {
           kcolor: "wk",
           hname: "F5",
-          mname: "F5"
+          mname: "F5",
+          kname: "v"
         },
         {
           kcolor: "bk",
           hname: "F<sup>#</sup>5",
-          mname: "F#5"
+          mname: "F#5",
+          kname: "↑v"
         },
         {
           kcolor: "wk",
           hname: "G5",
-          mname: "G5"
+          mname: "G5",
+          kname: "b"
         },
         {
           kcolor: "bk",
           hname: "G<sup>#</sup>5",
-          mname: "G#5"
+          mname: "G#5",
+          kname: "↑b"
         },
         {
           kcolor: "wk",
           hname: "A5",
-          mname: "A5"
+          mname: "A5",
+          kname: "n"
         },
         {
           kcolor: "bk",
           hname: "A<sup>#</sup>5",
-          mname: "A#5"
+          mname: "A#5",
+          kname: "↑n"
         },
         {
           kcolor: "wk",
           hname: "B5",
-          mname: "B5"
+          mname: "B5",
+          kname: "m"
         }
       ]
     };
@@ -232,14 +284,21 @@ export default {
         }
       }).toMaster();
       synth.triggerAttackRelease(mname, "8n");
+    },
+    handle: function () {
+      this.mode = -this.mode;
     }
   }
 };
 </script>
 
 <style lang="scss" scoped>
-.piano {
-  margin: 3em 0;
+.meau {
+  display: flex;
+  justify-content: center;
+}
+.keyboard {
+  margin: 3em 0 1em 0;
   display: flex;
   justify-content: center;
   align-items: flex-start;
